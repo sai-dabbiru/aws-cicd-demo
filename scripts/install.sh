f5b6475c7447b219681cb2cd9c78a20f471f1c9d
sai-dabbiru <149162359+sai-dabbiru@users.noreply.github.com>
2026-03-18 20:58:36 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,4 +3,5 @@ rm -rf /usr/share/nginx/html/*
 rm -f /usr/share/nginx/html/index.html
 dnf install -y nginx
 dnf install -y python3-pip
+dnf install -y libxml2-2.9.13
 pip3 install cryptography==36.0.0
