1b7dae8935bf68bacc331d6b5a5e036f5c4ae42a
sai-dabbiru <149162359+sai-dabbiru@users.noreply.github.com>
2026-04-15 17:32:04 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /usr/share/nginx/html
 cp -r /var/www/app/html/* /usr/share/nginx/html/ 2>/dev/null || \
-echo "<h1>Demo App - Vulnerable Build-2</h1>" > /usr/share/nginx/html/index.html
+echo "<h1>DevOps Pipeline Demo by Sai</h1>" > /usr/share/nginx/html/index.html
 systemctl enable nginx
 systemctl restart nginx
